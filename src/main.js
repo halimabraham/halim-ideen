0987a183b97b998c6283a480292e2571242597a8
Halim Abraham <halimcillo@gmail.com>
2023-08-23 18:58:59 -0600

    added vuetify, trashed useless files

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
-app.use(router)
+loadFonts()
 
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .use(pinia)
+  .mount('#app')
---
+++ src/views/HomeMovies.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{ msg }}</h1>
+    <button @click="mensaje('hola')">Click me</button>
+</template>
+
+<script setup>
+
+    const msg = 'Hellos'
+
+    const mensaje = (mensaje) => {
+        console.log(mensaje)
+    }
+
+</script>
